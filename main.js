1e48d9dd39b61f5253abe0bc7586730afe04d621
herom <jerome.manset22@gmail.com>
2022-11-24 00:11:09 +0800

    Add script for btn__arrow--active

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const listBtns = document.querySelectorAll('.faq__list--btn');
 const faqAnswers = document.querySelectorAll('.faq__answer');
+const arrowBtns = document.querySelectorAll('.btn__arrow');
 
 function resetList () {
     for (const faqAnswer of faqAnswers) {
     faqAnswer.classList.add('faq__answer--close');
+    }
+
+    for (const arrowBtn of arrowBtns) {
+        arrowBtn.classList.remove('btn__arrow--active');
     }
 }
 
@@ -12,6 +17,7 @@ for (let i = 0; i < listBtns.length; i++) {
     listBtns[i].addEventListener('click', function () {
     resetList();
     faqAnswers[i].classList.remove('faq__answer--close');
+    arrowBtns[i].classList.add('btn__arrow--active');
     
     }/* .bind(null, i) */);
 }
